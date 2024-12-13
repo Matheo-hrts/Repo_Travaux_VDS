45c327a7fc6553c845b9f27730537ac87c4ac9bb
Matheo-hrts <matheo.hertmans@hotmail.com>
2024-12-13 17:04:09 +0100

    Update script_csv.py

--- Dev/Script_perso/script_csv.py
+++ Dev/Script_perso/script_csv.py
@@ -5,6 +5,7 @@ import os
 
 
 class file_csv():
+
     def write_csv(self, file, name, quantity, price, category):
         """
         Préconditions:
@@ -17,8 +18,14 @@ class file_csv():
         - Ajoute une ligne avec les données spécifiées au fichier CSV indiqué.
         - Crée le fichier s'il n'existe pas.
         """
+        directory = 'CSV'
+
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+
         try:
-            with open(f'CSV/{file}', 'a', encoding='utf-8') as CSV_file:
+            with open(f'{directory}/{file}',
+                      'a', encoding='utf-8') as CSV_file:
                 CSV_file.write(f'{name},{quantity},{price},{category}\n')
 
         except IOError:
@@ -42,11 +49,10 @@ class file_csv():
                 i += 1
 
                 if i != 1:
-                    print(f'line {i} : name = {line[0]},' +
-                          f'quantity = {line[1]}, price = {line[2]}, ' +
-                          f'category = {line[3]}.')
+                    print(f'line {i} : name = {line[0]}, quantity = {line[1]},'
+                          + f'price = {line[2]}, category = {line[3]}.')
 
-    def merge_csv(self, directory, file_output):
+    def merge_csv(self, directory, directory_output, file_output):
         """
         Préconditions:
         - 'directory' est un chemin valide vers un répertoire contenant
@@ -61,6 +67,10 @@ class file_csv():
         if not os.path.exists(directory):
             raise FileNotFoundError('Le dossier n\'exitste pas')
 
+        if not os.path.exists(directory_output):
+            os.mkdir(directory_output)
+            print(f'the {directory_output} directory as been created')
+
         # Liste pour stocker les fichiers CSV
         file_csv = [f for f in os.listdir(directory) if f.endswith('.csv')]
 
@@ -69,8 +79,8 @@ class file_csv():
             raise FileNotFoundError('Aucun CSV dans le dossier')
 
         else:
-            with open(file_output, 'w', newline='',
-                      encoding='utf-8') as output:
+            with open(f'{directory_output}/{file_output}',
+                      'w', newline='', encoding='utf-8') as output:
                 output.write('name,quantity,price,category\n')
                 for file in file_csv:
                     file_path = os.path.join(directory, file)
@@ -109,7 +119,7 @@ class file_csv():
             self.write_csv(list_input[0], list_input[1],
                            list_input[2], list_input[3], list_input[4])
 
-    def sort_csv(self, file_input):
+    def sort_csv(self, directory_input, file_input):
         """
         Préconditions:
         - 'file_input' est un chemin valide vers un fichier CSV existant.
@@ -118,23 +128,28 @@ class file_csv():
 
         Postconditions:
         - Trie le fichier CSV selon la colonne spécifiée.
-        - Affiche les données triées et offre
-          la possibilité de les sauvegarder.
+        - Affiche les données triées et
+          offre la possibilité de les sauvegarder.
         """
-        sort_column = input('name quantity price category\n' +
-                            'On what do you want to sort ? : ')
+
+        sort_column = input('name quantity price category\n'
+                            + 'On what do you want to sort ? : ')
 
         if sort_column == 'exit':
             exit()
 
-        elif (sort_column != 'name' and sort_column != 'quantity' and
-                sort_column != 'price' and sort_column != 'category'):
-            print('Please enter an existinge column : ' +
-                  'name quantity price category\n')
+        elif (sort_column != 'name' and sort_column != 'quantity'
+                and sort_column != 'price' and sort_column != 'category'):
+            print('Please enter an existinge column : '
+                  + 'name quantity price category\n')
             self.sort_csv('merged_csv/merge.csv')
 
         else:
-            with open(file_input, 'r', encoding='utf-8') as file:
+            if not os.path.exists(directory_input):
+                raise FileNotFoundError('There is nothing to sort on')
+
+            with open(f'{directory_input}/{file_input}',
+                      'r', encoding='utf-8') as file:
                 reader = csv.DictReader(file)
                 lignes = list(reader)  # Charger toutes les lignes
                 # Trier les lignes en fonction de la colonne
@@ -144,19 +159,24 @@ class file_csv():
                 else:
                     sorted_lines = sorted(lignes, key=lambda x: x[sort_column])
                 for i in sorted_lines:
-                    print(f"name = {i['name']}, quantity = {i['quantity']}," +
-                          f" price = {i['price']}, " +
-                          f"category = {i['category']}.")
+                    print(f"name = {i['name']}, quantity = {i['quantity']},"
+                          + f" price = {i['price']}, "
+                          + f"category = {i['category']}.")
 
                 save = input('Do you want to save that sorted list ? : ')
-                if (save == '' or save == 'y' or save == 'Y' or
-                        save == 'yes' or save == 'Yes' or save == 'YES'):
+                if (save == 'y' or save == 'Y'
+                            or save == 'yes' or save == 'Yes'
+                            or save == 'YES'):
 
-                    file_output = input('What will be the name ' +
-                                        'of the file ? : ')
+                    file_output = input('What will be the name of '
+                                        + 'the file ? : ')
                     directory = 'sorted_csv'
-                    with open(f'{directory}/{file_output}.csv', 'w',
-                              newline='', encoding='utf-8') as file:
+                    if not os.path.exists(directory):
+                        os.mkdir(directory)
+                        print(f'The {directory} directory as been c')
+
+                    with open(f'{directory}/{file_output}.csv',
+                              'w', newline='', encoding='utf-8') as file:
                         writer = csv.DictWriter(file,
                                                 fieldnames=reader.fieldnames)
                         writer.writeheader()  # Écrire l'en-tête
@@ -171,14 +191,14 @@ class file_csv():
         - Exécute l'action correspondant à la commande.
         - Relance le menu en cas d'entrée incorrecte.
         """
-        first_input = input('read [r] write [w] merge [m] sort [s] ' +
-                            'quit [exit]\nWhat do you want to do ? : ')
+        first_input = input('read [r] write [w] merge [m] sort [s] quit [exit]'
+                            + '\nWhat do you want to do ? : ')
 
         if first_input == 'w':
             self.write_option()
 
         elif first_input == 'm':
-            self.merge_csv('CSV', 'merged_csv/merge.csv')
+            self.merge_csv('CSV', 'merged_csv', 'merge.csv')
 
         elif first_input == 'r':
             read = input('Which file do you want to read ? : ')
@@ -187,10 +207,11 @@ class file_csv():
                 self.print_csv(read)
 
             except FileNotFoundError:
+                print('The file that you typed does not exist')
                 self.what_to_do()
 
         elif first_input == 's':
-            self.sort_csv('merged_csv/merge.csv')
+            self.sort_csv('merged_csv', 'merge.csv')
 
         elif first_input == 'exit':
             exit()
